e95169639bcc795f74329aa723053f460065b001
SandraYushko <shyria6@gmail.com>
2023-04-10 22:20:44 +0300

    Commit - 1

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <
+          code>src/App.tsx</code> and save to reload.
         </p>
+        <p> Loren ipsum dolor. </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+           Learn React
         </a>
       </header>
     </div>
